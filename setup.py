54f672e91f6399a7245c079788d7d33e6887861a
Dawid Kostyszak <d.kostyszak@gmail.com>
2013-07-01 15:04:09 +0200

    first real commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='nokaut',
+    version='0.1',
+    description='Coroutine-based networking library',
+    author='Linden Lab',
+    author_email='sldev@lists.secondlife.com',
+    url='http://wiki.secondlife.com/wiki/Eventlet',
+    packages=['mama']
+)
