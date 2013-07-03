b3c1024ad8d9b7f3eb21799b97f09bc771b80c37
Dawid Kostyszak <d.kostyszak@gmail.com>
2013-07-03 12:17:52 +0200

    modified lib and tests in pep8 standard, and add tests

--- nokaut/lib.py
+++ nokaut/lib.py
@@ -4,22 +4,41 @@ import getopt
 from lxml import etree
 
 
-def nokaut_api(argv):
+def parser(argv):
+
+    product = ''
+    key = ''
 
     try:
-       opts, args = getopt.getopt(argv,"k",["keys="])
+        opts, args = getopt.getopt(argv, "k", ["keys="])
     except getopt.GetoptError:
-       print 'test.py -k <product> <key>'
-       sys.exit(2)
+        print 'test.py -k <product> <key>'
+        sys.exit(2)
+
     for opt, arg in opts:
-       if opt == '-h':
+        if opt == '-h':
             print 'test.py -k <product> <key>'
             sys.exit()
-       elif opt in ("-k", "--keys"):
+        elif opt in ("-k", "--keys"):
             product = args[0]
             key = args[1]
 
-    sock = urllib.urlopen("http://api.nokaut.pl/?format=xml&key="+key+"&method=nokaut.product.getByKeyword&keyword="+product+"&filters[price][from]=1200&filters[price][to]=2200")
+    if opts:
+        return product, key
+    else:
+        print 'test.py -k <product> <key>'
+        sys.exit(2)
+
+
+def nokaut_api(argv):
+
+    product, key = argv
+
+    sock = urllib.urlopen("http://api.nokaut.pl/?format=xml&key=" +
+                          key +
+                          "&method=nokaut.product.getByKeyword&keyword=" +
+                          product +
+                          "&filters[price][from]=1200&filters[price][to]=2200")
     htmlSource = sock.read()
     sock.close()
 
@@ -29,7 +48,7 @@ def nokaut_api(argv):
     path = root.xpath("node()")
     path2 = path[1].xpath("node()")
 
-    for i in range(1,len(path2),2):
+    for i in range(1, len(path2), 2):
         path3 = path2[i].xpath("node()")
         if path3[9].text < price:
             price = path3[9].text
@@ -38,5 +57,6 @@ def nokaut_api(argv):
     return price, url
 
 
-if __name__=="__main__": 
-    print nokaut_api(sys.argv[1:])
+if __name__ == "__main__":
+    parse = parser(sys.argv[1:])
+    print nokaut_api(parse)
--- nokaut/test.py
+++ nokaut/test.py
@@ -1,1 +1,31 @@
-import unittest.mock import magicMock
+from mock import MagicMock
+import unittest
+import lib
+
+
+class SerwerTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.test = lib
+
+    def tearDown(self):
+        self.test = None
+
+    def test1(self):
+        parser = MagicMock(name="parser", return_value=("laptop",
+                           "a8839b1180ea00fa1cf7c6b74ca01bb5"))
+        api = lib.nokaut_api(parser())
+        self.assertEqual(api, (
+                         '1249,00',
+                         'http://www.nokaut.pl/laptopy/toshiba-c850d-118.html'
+                         ))
+
+    def test2(self):
+        parser = lib.parser(["-k", "laptop",
+                             "a8839b1180ea00fa1cf7c6b74ca01bb5"])
+        self.assertEqual(parser, ("laptop",
+                         "a8839b1180ea00fa1cf7c6b74ca01bb5"))
+
+
+if __name__ == "__main__":
+    unittest.main()
